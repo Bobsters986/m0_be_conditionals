4c8bef9476b911f1754ae65415573f493302c8cf
Bobby Luly <bobbyluly@gmail.com>
2022-11-03 08:59:00 -0600

    Add spacer

--- code_challenges/medium_challenge.rb
+++ code_challenges/medium_challenge.rb
@@ -5,6 +5,7 @@ if good_driving_record == true && age >= 25
   p " You get a discount on the car rental"
 elsif good_driving_record == true || age >= 25
   p "You have to pay full price on the car rental"
-else good_driving_record == false && age <= 25
+else
+  
   p "You need someone else to sign for the rental"
 end
